c620c9d96eeb6fbb41af723f5c324c306489a959
Timofey <hunsolo94@gmail.com>
2024-12-28 09:13:00 +0300

    NEW

--- MainApp/views.py
+++ MainApp/views.py
@@ -135,12 +135,12 @@ def add_snippet_page(request):
 def comments_add(request):
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
-        if comment_form.is_valid:
+        if comment_form.is_valid():
             snippet_id = request.POST.get('snippet_id')
             snippet = Snippet.objects.get(id=snippet_id)
             comment = comment_form.save(commit=False)
             comment.author = request.user
             comment.snippet = snippet
             comment.save()
-            return redirect('sp_val', snippet_id=snippet.id)
+            return redirect('sp_val', snippet_id = snippet.id)
     return HttpResponseNotAllowed(['POST'])
